cd08401920073f7ed49ffab00528304b950ed17c
Aleksandr <tuzovap@gmail.ru>
2021-01-18 13:30:33 +0300

    test HomeWork 2.1

--- HomeWork2/HomeWork 2.1.py
+++ HomeWork2/HomeWork 2.1.py
@@ -1,1 +1,13 @@
-print('Hello World')
+# Создать список и заполнить его элементами различных типов данных.
+# Реализовать скрипт проверки типа данных каждого элемента.
+# Использовать функцию type() для проверки типа.
+# Элементы списка можно не запрашивать у пользователя, а указать явно, в программе.
+
+
+result_list = [2, 'text', 456, 45.3, None]
+
+def type_checking (list_elements):
+    for element in list_elements:
+        print(type(element))
+
+type_checking(result_list)
